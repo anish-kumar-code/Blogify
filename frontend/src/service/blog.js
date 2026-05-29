1fe9ac16249dd090fc9730e91799b8018a3e172f
Anish <anishkr2842003@gmail.com>
2026-05-29 10:03:18 +0530

    live url add

--- frontend/src/service/blog.js
+++ frontend/src/service/blog.js
@@ -2,22 +2,22 @@ import axios from 'axios';
 
 
 export const addBlog = async (data) => {
-    let response = await axios.post('http://localhost:3000/addblog', data)
+    let response = await axios.post('https://blogify-u9g6.onrender.com/addblog', data)
     return response;
 }
 
 
 export const getAllBlog = async () => {
-    let response = await axios.get('http://localhost:3000/allblog')
+    let response = await axios.get('https://blogify-u9g6.onrender.com/allblog')
     return response;
 }
 
 export const getSingleBlogDetails = async (id) => {
-    let response = await axios.get(`http://localhost:3000/getblogbyid/${id}`)
+    let response = await axios.get(`https://blogify-u9g6.onrender.com/getblogbyid/${id}`)
     return response;
 }
 
 export const editBlog = async (id, data) => {
-    let response = await axios.patch(`http://localhost:3000/editblog/${id}`, data)
+    let response = await axios.patch(`https://blogify-u9g6.onrender.com/editblog/${id}`, data)
     return response;
 }
